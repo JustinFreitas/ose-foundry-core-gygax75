9998737aea3ad1c60bf8d86963a48ffa874b37a9
Justin Freitas <justin.s.freitas@me.com>
2026-07-04 13:18:32 -0600

    test: configure code coverage using @vitest/coverage-v8

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,5 +6,17 @@ export default defineConfig({
     setupFiles: ["./test/setup.ts"],
     include: ["src/**/*.spec.ts"],
     exclude: ["node_modules", "dist"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      include: ["src/**/*.ts", "src/**/*.js"],
+      exclude: [
+        "src/**/*.test.ts",
+        "src/**/*.spec.ts",
+        "src/e2e/**/*",
+        "src/module/__tests__/**/*",
+        "test/**/*",
+      ],
+    },
   },
 });
